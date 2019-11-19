91d734dd90618632f47b6a36ded68b207430533d
Richard Aguirre <richardjap@gmail.com>
2019-11-19 16:50:02 -0500

    logeo

--- src/app/main-login/main-login.component.ts
+++ src/app/main-login/main-login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { logging } from 'protractor';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthServiceService } from '../guards/auth-service.service';
 import { routerNgProbeToken } from '@angular/router/src/router_module';
 import { LoaderComponent } from '../loader/loader.component';
@@ -15,38 +15,38 @@ import { LoaderComponent } from '../loader/loader.component';
 export class MainLoginComponent implements OnInit {
   usuario: string;
   clave: string;
-  router:Router;
-  userError:boolean=false;
-  loaderActive:boolean=false;
-  constructor( router: Router, private authService: AuthServiceService) { 
-    this.router=router;
+  router: Router;
+  userError: boolean = false;
+  loaderActive: boolean = false;
+  constructor(router: Router, private authService: AuthServiceService) {
+    this.router = router;
   }
 
   ngOnInit() {
   }
 
-  login(){
-  this.loaderActive=true;
-   let request=this.authService.login(this.usuario,this.clave);
-    /*
-   this.authService.login(this.usuario,this.clave).subscribe((data) =>{
-    console.log(data.headers);
-    if (data.Success){*/
-      this.authService.setAuth("true");
-      
-      this.router.navigate(['/home']);
-      /*
-    }else{
-      this.userError=true;
-      
-    }
-    this.loaderActive=false;
-    
-   },(error) =>{
-    console.log(error);
-    this.usuario='';
-    this.clave='';
-   });*/
+  login() {
+    this.loaderActive = true;
+    let request = this.authService.login(this.usuario, this.clave);
+
+    this.authService.login(this.usuario, this.clave).subscribe((data) => {
+      console.log(data.headers);
+      if (data.Success) {
+        this.authService.setAuth('true');
+
+        this.router.navigate(['/home']);
+
+      } else {
+        this.userError = true;
+
+      }
+      this.loaderActive = false;
+
+    }, (error) => {
+      console.log(error);
+      this.usuario = '';
+      this.clave = '';
+    });
   }
 
 }
